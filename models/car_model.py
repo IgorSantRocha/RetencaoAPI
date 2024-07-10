b4a80e44d8c2cfb3ba21ccaf398ec3512f1fe683
IgorSantRocha <igor77876@gmail.com>
2024-07-10 10:55:54 -0300

    Removidas todas as referências ao Model CAR usado como base

--- models/car_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from db.base_class import Base
-
-
-class Car(Base):
-    __tablename__ = 'TB_Car'
-    id = Column(Integer, primary_key=True, index=True)
-    model = Column(String(255), index=True)
-    year = Column(Integer, index=True)
