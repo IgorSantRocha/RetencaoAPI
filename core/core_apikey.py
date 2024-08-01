52293f854132386b19806cb7e2e135a1642b8852
IgorSantRocha <igor77876@gmail.com>
2024-08-01 12:23:26 -0300

    Movidas configurações do APIKey para o arquivo de config.py

--- core/core_apikey.py
+++ core/core_apikey.py
@@ -1,17 +1,12 @@
 from fastapi import FastAPI, Depends, HTTPException, Security, status
-from fastapi.security.api_key import APIKeyHeader, APIKey
+from core.config import settings
 from sqlalchemy.orm import Session
 from crud.crud_tb_retencaoapi_tokens import tb_retencaoapi_tokens
 from api import deps
 
-# substitua pela sua chave API real
-API_KEY = "K90nIR4PbYaTY4JR$qhBKIy0rPZ6uwSqKCDX"
-API_KEY_NAME = "access_token"
-api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
-
-async def get_api_key(api_key_header: str = Security(api_key_header)):
-    if api_key_header == API_KEY:
+async def get_api_key(api_key_header: str = Security(settings.api_key_header)):
+    if api_key_header == settings.API_KEY:
         return api_key_header
     else:
         raise HTTPException(
