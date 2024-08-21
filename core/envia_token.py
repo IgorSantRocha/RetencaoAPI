22654d35a821c452e1b54c725cb9625321299b49
IgorSantRocha <igor77876@gmail.com>
2024-08-21 09:16:06 -0300

    Criado o método de envio por e-mail

--- core/envia_token.py
+++ core/envia_token.py
@@ -2,6 +2,9 @@ from fastapi import HTTPException, status
 from core.request import RequestEvolutionAPI
 from schemas.auth_schema import AuthTokenVerficicacaoCreate, AuthTokenVerficicacaoResponse
 from utils import format_whatsapp_number
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class EnviaToken(AuthTokenVerficicacaoCreate):
@@ -34,7 +37,46 @@ class EnviaToken(AuthTokenVerficicacaoCreate):
         return response
 
     async def _email(self, token: int):
-        pass
+        # Configurações do servidor
+        smtp_host = 'smtp.hostinger.com'
+        smtp_port = 587
+        email_user = 'sys@c-trends.com.br'
+        email_pass = 'Cr@$#2020'
+
+        # Configuração do email
+        de = 'sys@c-trends.com.br'
+        para = self.email
+        assunto = 'Token de alteração de senha'
+        corpo = f"""
+        <html>
+        <head></head>
+        <body>
+            <h1>Olá!</h1>
+            <p>Anote o token de alteração de senha: <b style="color: blue;">{token}</b>!</p>
+        </body>
+        </html>
+        """
+
+        # Criando a mensagem
+        msg = MIMEMultipart()
+        msg['From'] = de
+        msg['To'] = para
+        msg['Subject'] = assunto
+
+        # Adicionando o corpo do email
+        msg.attach(MIMEText(corpo, 'html'))
+
+        # Enviando o email
+        try:
+            server = smtplib.SMTP(smtp_host, smtp_port)
+            server.starttls()
+            server.login(email_user, email_pass)
+            texto = msg.as_string()
+            server.sendmail(de, para, texto)
+            server.quit()
+            print("Email enviado com sucesso!")
+        except Exception as e:
+            print(f"Falha ao enviar o email: {e}")
 
     async def _sms(self, token: int):
         pass
