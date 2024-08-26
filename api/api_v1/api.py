28a2cc40a511f9995bcd2d67333957b59128807a
IgorSantRocha <igor77876@gmail.com>
2024-08-26 09:39:17 -0300

    Ajustada ordem dos endpoints na documentação

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -4,16 +4,16 @@ from api.api_v1.endpoints import consultas, abertura, auth, firebase, respostas_
 
 api_router = APIRouter()
 api_router.include_router(
-    consultas.router, prefix="/consultas", tags=["Consultas"])
-
-api_router.include_router(
-    abertura.router, prefix="/abertura", tags=["Abertura"])
-
-api_router.include_router(
     auth.router, prefix="/auth", tags=["Autenticação"])
 
 api_router.include_router(
+    consultas.router, prefix="/consultas", tags=["Consultas"])
+
+api_router.include_router(
     firebase.router, prefix="/firebase", tags=["Firebase Upload"])
+
+api_router.include_router(
+    abertura.router, prefix="/abertura", tags=["Abertura"])
 
 api_router.include_router(
     respostas_sga.router, prefix="/respostas", tags=["Respostas SGA"])
